966b962ee479644fa0d4b878b42c28d34bcbe49c
Carlos Zepeda <cjzepedaa@gmail.com>
2021-03-20 16:07:56 -0600

    Added new pages , refracted code, added more responsive designs
    For my CSS file I refracted code removed unnecessary classes and combined other classes together. I also added another page to the site removing that section from the first page entirely onto the new page. I changed the styling of the page to fit a darker theme and added some media queries for more responsive design.

---
+++ dicen.js
@@ -0,0 +1,8 @@
+/*this makes nav sticky*/
+
+window.addEventListener("scroll",function() {
+  var navbar = document.querySelector("#navbar");
+  navbar.classList.toggle("sticky" , window.scrollY > 0)
+  
+})
+
