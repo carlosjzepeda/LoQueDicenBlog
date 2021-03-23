4dc2ce9013b0c6939c13668b5823435d2775592b
Carlos Zepeda <cjzepedaa@gmail.com>
2021-03-23 14:23:18 -0600

    Added more imgs and more responsive designs alongwith layout changes
    html
    main page and editorial page added new layouts and more images
    
    css 
    made less redundant classes removed lines that aren't unnecessary added responsive designs

--- dicen.js
+++ dicen.js
@@ -2,6 +2,7 @@
 
 window.addEventListener("scroll", function () {
   let navbar = document.querySelector("#navbar");
-  navbar.classList.toggle("sticky", window.scrollY > 0)
+  navbar.classList.toggle("sticky-nav", window.scrollY > 0)
+
 
 })
